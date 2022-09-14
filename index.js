4566fff2f0ac7d265a98d1a325379578978557ea
Ngufor-emmanuella <emmanuellangufor@gmail.com>
2022-09-14 14:34:17 +0100

    Add elements to html

--- index.js
+++ index.js
@@ -19,3 +19,37 @@ links.addEventListener('click', () => {
   links.style.display='none';
   exit.style.display='none';
 })
+
+const carttt = document.querySelector('.carttt');
+const pens = [{
+  heading: "Profesional Art",
+  subHeading: "Printing Data",
+  paragragh: "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard",
+  
+
+}
+
+]
+pens.forEach((pen) => {
+  const first = `
+  <div class="cart" id="cf1">
+                <h2>${pen.heading}<span>${pen.subHeading}</span></h2>
+                    
+                    <ul>
+                        <li>Html</li>
+                        <li>Bootstrap</li>
+                        <li>Ruby</li>
+                    </ul>
+                    <button type="button" name="See-Project">See Project</button>
+            </div>`;
+            carttt.innerHtml += first;
+});
+
+
+
+
+
+
+
+
+
