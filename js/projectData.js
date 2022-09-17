45000c0e55132ee84c8e33f2534e0120acb65980
Ngufor-emmanuella <emmanuellangufor@gmail.com>
2022-09-17 22:53:10 +0100

    Fix errors in css

---
+++ js/projectData.js
@@ -0,0 +1,69 @@
+const data = [
+
+  {
+    id: 1,
+    title: 'Data Dashboard Healthcare',
+    description:
+      'A daily selection of privately personalized reads no accounts or sign-ups required.has been the industry standard',
+    lang: ['html', 'Bootstrap', 'Ruby'],
+    src: '../img/SnapshootPortfolio.jpg',
+    live: 'https://blase147.github.io/Mobile-first-Portfolio-Project',
+    source: 'https://github.com/blase147/Mobile-first-Portfolio-Project',
+  },
+
+  {
+    id: 2,
+    title: 'Data Dashboard Healthcare',
+    description:
+      'A daily selection of privately personalized reads no accounts or sign-ups required.has been the industry standard',
+    lang: ['html', 'Bootstrap', 'Ruby'],
+    src: '../img/bg-image2.jpg',
+    live: 'https://blase147.github.io/Mobile-first-Portfolio-Project',
+    source: 'https://github.com/blase147/Mobile-first-Portfolio-Project',
+  },
+
+  {
+    id: 3,
+    title: 'Website Portfolio',
+    description:
+      'A daily selection of privately personalized reads no accounts or sign-ups required.has been the industry standard',
+    lang: ['html', 'Bootstrap', 'Ruby'],
+    src: '../img/bg-image3.jpg',
+    live: 'https://blase147.github.io/Mobile-first-Portfolio-Project',
+    source: 'https://github.com/blase147/Mobile-first-Portfolio-Project',
+  },
+
+  {
+    id: 4,
+    title: 'Professional Art Printing Data',
+    description:
+      'A daily selection of privately personalized reads no accounts or sign-ups required.has been the industry standard',
+    lang: ['html', 'Bootstrap', 'Ruby'],
+    src: '../img/bg-image4.png',
+    live: 'https://blase147.github.io/Mobile-first-Portfolio-Project',
+    source: 'https://github.com/blase147/Mobile-first-Portfolio-Project',
+  },
+
+  {
+    id: 5,
+    title: 'Data Dashboard Healthcare',
+    description:
+      'A daily selection of privately personalized reads no accounts or sign-ups required.has been the industry standard',
+    lang: ['html', 'Bootstrap', 'Ruby'],
+    src: '../img/bg-image2.jpg',
+    live: 'https://blase147.github.io/Mobile-first-Portfolio-Project',
+    source: 'https://github.com/blase147/Mobile-first-Portfolio-Project',
+  },
+
+  {
+    id: 6,
+    title: 'Website Portfolio',
+    description:
+      'A daily selection of privately personalized reads no accounts or sign-ups required.has been the industry standard',
+    lang: ['html', 'Bootstrap', 'Ruby'],
+    src: '../img/bg-image3.jpg',
+    live: 'https://blase147.github.io/Mobile-first-Portfolio-Project',
+    source: 'https://github.com/blase147/Mobile-first-Portfolio-Project',
+  },
+];
+export default data; // export default data;
